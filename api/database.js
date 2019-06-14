9f9e0fe3c4f22143a3278298e9b9f430a234b66c
Mike Anderson <mbanderson@uwalumni.com>
2019-06-14 16:15:25 +0000

    Add initial mongodb interaction
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>

---
+++ api/database.js
@@ -0,0 +1,13 @@
+const config = require('../config.json')
+
+const MongoClient = require('mongodb').MongoClient
+const client = new MongoClient(config.mongodb_uri, { useNewUrlParser: true })
+client.connect(err => {
+    if (err) {
+	console.log("Error connecting to MongoDB")
+	process.exit(1)
+    }
+    console.log("Connected to MongoDB")
+    const collection = client.db("test").collection("devices")
+    client.close()
+})
