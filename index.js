6fafeb444c1d10241eb5638b4f93e8b05e066e43
Mike Anderson <mbanderson@uwalumni.com>
2019-06-19 19:23:17 +0000

    Allow cross-site requests
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+// Allow cross-site origin requests.
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 var router = require('./api/router')
 
 // Start all router paths with /api
