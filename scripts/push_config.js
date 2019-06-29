d6404ecb3b43c103653669a9b4b8ed2a8eb24fa8
Mike Anderson <mbanderson@uwalumni.com>
2019-06-28 20:35:56 -0500

    fix many lint errors
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>

--- scripts/push_config.js
+++ scripts/push_config.js
@@ -7,7 +7,7 @@ const mongodb = require('mongodb')
 const client = new mongodb.MongoClient(config.mongodb_uri, { useNewUrlParser: true })
 const db_name = 'boardfarm'
 
-if (process.argv.length != 3) {
+if (process.argv.length !== 3) {
   console.log('You must specify a .json file to upload into mongodb')
   process.exit(1)
 }
@@ -27,22 +27,22 @@ client.connect(err => {
   console.log('Connected to MongoDB')
 
   // Collections to insert to
-  station_coll = client.db(db_name).collection('station')
-  location_coll = client.db(db_name).collection('location')
-  device_coll = client.db(db_name).collection('device')
+  var station_coll = client.db(db_name).collection('station')
+  var location_coll = client.db(db_name).collection('location')
+  var device_coll = client.db(db_name).collection('device')
 
   // Array of documents we will insert
-  stations_to_insert = []
-  locations_to_insert = []
-  devices_to_insert = []
+  var stations_to_insert = []
+  var locations_to_insert = []
+  var devices_to_insert = []
 
   console.log(Object.keys(bf_config))
-  if ("locations" in bf_config) {
+  if ('locations' in bf_config) {
     var entries = Object.entries(bf_config.locations)
     for (const [key, val] of entries) {
       // Put shared devices into their own table
-      if ("devices" in val) {
-        val["devices"].forEach(e => {
+      if ('devices' in val) {
+        val['devices'].forEach(e => {
           e.max_users = 1
           e.location = key
           devices_to_insert.push(e)
@@ -57,15 +57,15 @@ client.connect(err => {
       if (err) {
         throw err
       }
-      console.log("Inserted %s devices.", devices_to_insert.length)
+      console.log('Inserted %s devices.', devices_to_insert.length)
       process.exit(0)
     })
 
     location_coll.insertMany(locations_to_insert, (err, res) => {
       if (err) {
-	throw err
+        throw err
       }
-      console.log("Inserted %s locations.", locations_to_insert.length)
+      console.log('Inserted %s locations.', locations_to_insert.length)
     })
   }
 
@@ -73,10 +73,10 @@ client.connect(err => {
   for (const [key, val] of entries) {
     val.name = key
     val.active_users = 0
-    val.active_user = ""
-    val.active_host = ""
-    val.prev_user = ""
-    val.prev_host = ""
+    val.active_user = ''
+    val.active_host = ''
+    val.prev_user = ''
+    val.prev_host = ''
     val.total_uses = 0
     stations_to_insert.push(val)
   }
@@ -84,12 +84,12 @@ client.connect(err => {
     if (err) {
       throw err
     }
-    console.log("Inserted %s stations.", stations_to_insert.length)
+    console.log('Inserted %s stations.', stations_to_insert.length)
   })
 
-  console.log("Closing connection ...")
+  console.log('Closing connection ...')
   client.close()
-  /*collection.insertOne(bf_config, (err, res) => {
+  /* collection.insertOne(bf_config, (err, res) => {
     if (err) {
       console.log(err)
       console.log('Error inserting config into MongoDB')
@@ -97,5 +97,5 @@ client.connect(err => {
     }
     console.log('Inserted %s into MongodDB', bf_config_name)
     client.close()
-  })*/
+  }) */
 })
