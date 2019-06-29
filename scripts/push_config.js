55e19ae94a139ebfb7ffee900464c1c1ad04705e
Mike Anderson <mbanderson@uwalumni.com>
2019-06-28 21:22:15 -0500

    Make push_config read fully first, then insert to mongo
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>

--- scripts/push_config.js
+++ scripts/push_config.js
@@ -1,42 +1,17 @@
 #!/usr/bin/env node
 
+const mongodb = require('mongodb')
 const config = require('../config')
 
-const mongodb = require('mongodb')
-// const MongoClient = require('mongodb').MongoClient
-const client = new mongodb.MongoClient(config.mongodb_uri, { useNewUrlParser: true })
-const db_name = 'boardfarm'
-
-if (process.argv.length !== 3) {
-  console.log('You must specify a .json file to upload into mongodb')
-  process.exit(1)
-}
-var bf_config_name = process.argv[2]
-if (!bf_config_name.startsWith('.')) {
-  bf_config_name = './' + bf_config_name
-}
-console.log('Loading %s', bf_config_name)
-const bf_config = require(bf_config_name)
-
-client.connect(err => {
-  if (err) {
-    console.log(err)
-    console.log('Error connecting to MongoDB')
-    process.exit(1)
-  }
-  console.log('Connected to MongoDB')
-
-  // Collections to insert to
-  var station_coll = client.db(db_name).collection('station')
-  var location_coll = client.db(db_name).collection('location')
-  var device_coll = client.db(db_name).collection('device')
+function process_config (bf_config_name, callback) {
+  console.log('Reading %s ...', bf_config_name)
+  const bf_config = require(bf_config_name)
 
   // Array of documents we will insert
-  var stations_to_insert = []
-  var locations_to_insert = []
   var devices_to_insert = []
+  var locations_to_insert = []
+  var stations_to_insert = []
 
-  console.log(Object.keys(bf_config))
   if ('locations' in bf_config) {
     var entries = Object.entries(bf_config.locations)
     for (const [key, val] of entries) {
@@ -47,29 +22,15 @@ client.connect(err => {
           e.location = key
           devices_to_insert.push(e)
         })
+        delete val.devices
       }
       val.name = key
       locations_to_insert.push(val)
     }
     delete bf_config.locations
-
-    device_coll.insertMany(devices_to_insert, (err, res) => {
-      if (err) {
-        throw err
-      }
-      console.log('Inserted %s devices.', devices_to_insert.length)
-      process.exit(0)
-    })
-
-    location_coll.insertMany(locations_to_insert, (err, res) => {
-      if (err) {
-        throw err
-      }
-      console.log('Inserted %s locations.', locations_to_insert.length)
-    })
   }
 
-  var entries = Object.entries(bf_config)
+  entries = Object.entries(bf_config)
   for (const [key, val] of entries) {
     val.name = key
     val.active_users = 0
@@ -80,22 +41,65 @@ client.connect(err => {
     val.total_uses = 0
     stations_to_insert.push(val)
   }
-  station_coll.insertMany(stations_to_insert, (err, res) => {
-    if (err) {
-      throw err
-    }
-    console.log('Inserted %s stations.', stations_to_insert.length)
-  })
 
-  console.log('Closing connection ...')
-  client.close()
-  /* collection.insertOne(bf_config, (err, res) => {
+  callback(devices_to_insert, locations_to_insert, stations_to_insert)
+}
+
+function insert_into_mongo (devices_to_insert, locations_to_insert, stations_to_insert) {
+  console.log('Will insert into mongodb:')
+  console.log(' * %s devices', devices_to_insert.length)
+  console.log(' * %s locations', locations_to_insert.length)
+  console.log(' * %s stations', stations_to_insert.length)
+
+  // const MongoClient = require('mongodb').MongoClient
+  const client = new mongodb.MongoClient(config.mongodb_uri, { useNewUrlParser: true })
+  const db_name = 'boardfarm'
+
+  client.connect(err => {
     if (err) {
       console.log(err)
-      console.log('Error inserting config into MongoDB')
+      console.log('Error connecting to MongoDB')
       process.exit(1)
     }
-    console.log('Inserted %s into MongodDB', bf_config_name)
-    client.close()
-  }) */
-})
+    console.log('Connected to MongoDB')
+
+    // Collections to insert to
+    var station_coll = client.db(db_name).collection('station')
+    var location_coll = client.db(db_name).collection('location')
+    var device_coll = client.db(db_name).collection('device')
+
+    device_coll.insertMany(devices_to_insert, (err, res) => {
+      if (err) {
+        throw err
+      }
+      console.log('Inserted %s devices.', devices_to_insert.length)
+      process.exit(0)
+    })
+    location_coll.insertMany(locations_to_insert, (err, res) => {
+      if (err) {
+        throw err
+      }
+      console.log('Inserted %s locations.', locations_to_insert.length)
+    })
+    station_coll.insertMany(stations_to_insert, (err, res) => {
+      if (err) {
+        throw err
+      }
+      console.log('Inserted %s stations.', stations_to_insert.length)
+    })
+    console.log('Closing connection ...')
+    setTimeout(client.close, 3000)
+  })
+}
+
+if (require.main === module) {
+  if (process.argv.length !== 3) {
+    console.log('You must specify a .json file to upload into mongodb')
+    process.exit(1)
+  }
+  var bf_config_name = process.argv[2]
+  if (!bf_config_name.startsWith('.')) {
+    bf_config_name = './' + bf_config_name
+  }
+  process_config(bf_config_name, insert_into_mongo)
+}
